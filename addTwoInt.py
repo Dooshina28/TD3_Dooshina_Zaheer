24f594036a70f38503dcbd77dbf5b2d03a28a87c
Zaheer180899 <zaheerariadne@gmail.com>
2020-03-09 14:51:31 +0400

    My third commit from branch add

--- addTwoInt.py
+++ addTwoInt.py
@@ -6,11 +6,17 @@ def add(a,b):
 
 def main():
 	if (len(sys.argv) > 3):
-		print("Erreur, plus de deux arguments")
+		print("Erreur, il y a", ((len(sys.argv)) - 3), " plus de arguments")
 		sys.exit(1)
-	elif (len(sys.argv) < 3):
-		print("Erreur, moins de deux arguments")
-		sys.exit(1)
+	elif (len(sys.argv) == 1):
+		print("Erreur, il manque ", (3 - (len(sys.argv))), "  arguments")
+		x = int(input("Entrez la valeur du premier argument: "))
+		y = int(input("Entrez la valeur du deuxieme argument: "))
+		print(add(int(x),int(y)))
+	elif (len(sys.argv) == 2):
+		print("Erreur, il manque", (3 - (len(sys.argv))), "  arguments")
+		y = int(input("Entrez la valeur du deuxieme argument: "))
+		print(add(int(sys.argv[1]),int(y)))
 	else:
 		x = sys.argv[1]
 		y = sys.argv[2]
