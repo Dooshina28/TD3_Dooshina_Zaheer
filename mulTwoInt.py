c920b3b26e3a1a1848ad82d701b602d9e27e18a8
Dooshina28 <dmootoocurpen@gmail.com>
2020-03-09 15:03:13 +0400

    Fonction input pour les variables manquantes

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -7,8 +7,16 @@ def mul(a,b):
 def main():
 	if (len(sys.argv) > 3):
 		print("Erreur, il y a plus de deux arguments")
-	elif (len(sys.argv) < 3):
-		print("Erreur, il y a moins de deux arguments")
+	elif (len(sys.argv) == 1):
+		print("Erreur, il manque" , (3 - (len(sys.argv))) , "arguments")
+		x = int(input("Entrez le premier argument: "))
+		y = int(input("Entrez le deuxieme argument: "))
+		print(mul(int(x),int(y)))
+	elif (len(sys.argv) == 2):
+		print("Erreur, il manque" , (3 - (len(sys.argv))) , "arguments")
+		y = int(input("Entrez le deuxieme arguments: "))
+		x = sys.argv[1]
+		print(mul(int(x),int(y)))
 	else:
 		x = sys.argv[1]
 		y = sys.argv[2]
