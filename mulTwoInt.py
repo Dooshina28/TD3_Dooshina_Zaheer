e32a720cbc19d4a39611746fcf0045a87d4896ad
Dooshina28 <dmootoocurpen@gmail.com>
2020-03-09 14:51:37 +0400

    Message d'erreur

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -5,8 +5,13 @@ def mul(a,b):
 	return x
 
 def main():
-	x = sys.argv[1]
-	y = sys.argv[2]
-	print(mul(int(x),int(y)))
+	if (len(sys.argv) > 3):
+		print("Erreur, il y a plus de deux arguments")
+	elif (len(sys.argv) < 3):
+		print("Erreur, il y a moins de deux arguments")
+	else:
+		x = sys.argv[1]
+		y = sys.argv[2]
+		print(mul(int(x),int(y)))
 
 main()
