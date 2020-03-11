ae8ba04496019194bb68dbb870e95e7742887453
Dooshina28 <dmootoocurpen@gmail.com>
2020-03-11 17:29:20 +0400

    final commit

--- main.py
+++ main.py
@@ -2,3 +2,6 @@
 x = input("Voulez-vous multiplier deux chiffres, repondez par oui si vous le voulez? ")
 if (x == "oui"):
 	import mulTwoInt
+y = input("Voulez-vous ajoutez deux chiffres, repondez par oui si vous le voulez? ")
+if (y == "oui"):
+	import addTwoInt
